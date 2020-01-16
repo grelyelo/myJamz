ed9ee1edf74d0b1227992b412f389810726a565e
Timothy L <tlesgaux@protonmail.ch>
2020-01-16 12:55:45 -0600

    added initial ui.js, which will be used to fill the UI elements dynamically

---
+++ public/ui.js
@@ -0,0 +1,17 @@
+//Add event listener to search bar to update content as user types
+//Every time the user submits a keystroke, we should do a search on the API. 
+//Extra feature: perhaps wait at least 1 second after keystroke before searching. 
+
+//Add event listener to sidebar elements to display appropriate content.
+//onclick, load the page. 
+$( "#home" ).click(function() {
+    alert( "Handler for .click() called on #about element" );
+});
+
+$( "#browse" ).click(function() {
+    alert( "Handler for .click() called on #browse element" );
+});
+
+$( "#radio" ).click(function() {
+    alert( "Handler for .click() called on #radio element" );
+});
