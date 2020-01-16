e7351e55f135ec5227a01314f56e91f44b079dc6
Timothy L <tlesgaux@protonmail.ch>
2020-01-16 13:52:42 -0600

    added code to load songs when #home elem is clicked

--- public/ui.js
+++ public/ui.js
@@ -5,7 +5,17 @@
 //Add event listener to sidebar elements to display appropriate content.
 //onclick, load the page. 
 $( "#home" ).click(function() {
-    alert( "Handler for .click() called on #about element" );
+    //For now, we shall perform a search for the songs, 
+    console.log("event fired")
+    $("#songs").empty();
+    $.getJSON("/songs", function(songs) {
+        console.log("fuck you")
+        for(var i = 0; i < songs.length; i++){
+            $("#songs").append('<li>' + songs[i].artist  + " - " + songs[i].title + '</li>')
+        }
+    });    
+    //parse the json formatted data,
+    //and render the elements
 });
 
 $( "#browse" ).click(function() {
